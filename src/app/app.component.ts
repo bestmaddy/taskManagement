ec9b35aabfa990faf14176fffd8055b8269d84f0
mandeep <mandeepwadekar@gmail.com>
2023-09-22 17:11:16 +0530

    task management app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ServiceService } from './service/service.service';
+import { TaskData } from './modules/taskData';
+import { NgForm } from '@angular/forms';
+import { take } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('TaskForm', { static: true })
+  TaskForm!: NgForm;
   title = 'TaskManagement';
+  taskData: TaskData[] | undefined;
+  createTask = new TaskData;
+  tatus: any[] = [
+    { id: '1', Status: 'Running' },
+    { id: '2', Status: 'Closed' },
+    { id: '3', Status: 'Cancelled' }
+  ]
+  constructor(private service: ServiceService) {
+    this.getTask();
+
+  }
+  ngOnInit(): void {
+  }
+  getTask() {
+    this.service.getTask().subscribe((data: any) => {
+      console.log("taskData", data)
+      this.taskData = data.data
+    })
+  }
+  ChangeStatus(data: any, Status: string) {
+    console.log(data, Status)
+    this.service.updateTask({ "id": data, "status": Status });
+    this.getTask();
+  }
+  DeleteTask(id: number | undefined) {
+    console.log("delete id", id)
+    this.service.deleteTask(id);
+    this.getTask();
+  }
+  CreateTask(task: any) {
+    console.log("task form", JSON.stringify(task.value))
+    if (task.valid) {
+      this.TaskForm.resetForm()
+      this.service.saveTask(task).subscribe((data:any) => {
+        console.log("data form save api:", data)
+      })
+      this.getTask();
+    }
+  }
+  reset(taskform: NgForm) {
+    taskform.resetForm();
+  }
 }
---
+++ src/app/service/service.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  constructor(private httpClient: HttpClient) { }
+
+  getTask(): Observable<any[]> {
+    return this.httpClient.get<any[]>("http://localhost:3000/getTask")
+      .pipe(
+        catchError(this.errorHandler)
+      )
+  }
+  saveTask(data:any): Observable<any[]> {
+    const saveTask = this.httpClient.post<any[]>("http://localhost:3000/SaveTask", data, this.httpOptions)
+      .pipe(
+        catchError(this.errorHandler)
+      )
+      return (saveTask)
+  }
+  updateTask(data:any): Observable<any[]> {
+    console.log(data)
+    const updateTask = this.httpClient.put<any[]>("http://localhost:3000/updateTask", data)
+      .pipe(
+        catchError(this.errorHandler)
+      )
+      return(updateTask)
+  }
+  deleteTask(data:any): Observable<any> {
+    const deleteTask = this.httpClient.delete<number>(`http://localhost:3000/deleteTask?id=${data}`, this.httpOptions)
+      .pipe(
+        catchError(this.errorHandler)
+      )
+      return(deleteTask)
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+}
